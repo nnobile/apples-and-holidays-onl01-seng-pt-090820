9584f15dc1a28b3c27a20d53b65a4b7235f35bea
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 23:54:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,8 @@ holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |day, item|
   puts "  #{day.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-  end
+end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
