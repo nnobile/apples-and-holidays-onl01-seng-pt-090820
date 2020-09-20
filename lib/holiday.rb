a3b1e726ca4acb3e571c03d241e1a63ac08ab789
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 23:57:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,20 +51,18 @@ end
 # return an array of all of the supplies that are used in the winter season
 
 def all_supplies_in_holidays(holiday_hash)
-holiday_hash.each do |season, holiday|
-  puts "#{season.capitalize}:"
-  holiday.each do |day, item|
-  puts "  #{day.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
 end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-holiday_hash.map do |season, holidays|
-  holidays.map do |holiday, supplies|
-    holiday if supplies.include?("BBQ")
-  end
-end
-end.flatten
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten
 end
